b695cf5e302e6eff74c8516ee3925f8c7578da7d
mingriyuan <dekaidong231@gmail.com>
2025-04-10 01:36:20 +0800

    修改阵营成员数

--- src/main/java/net/micmu/mcmods/micwands/core/ModConfig.java
+++ src/main/java/net/micmu/mcmods/micwands/core/ModConfig.java
@@ -7,7 +7,7 @@ public class ModConfig {
     @net.minecraftforge.common.config.Config.Comment("是否移除苦力怕膨胀AI（默认关闭）")
     public static boolean removeCreeperSwellAI = false;
     
-    @net.minecraftforge.common.config.Config.Comment("阵营最大成员数量(1-20)")
-    @net.minecraftforge.common.config.Config.RangeInt(min = 1, max = 20)
-    public static int factionMaxMembers = 5;
+    @net.minecraftforge.common.config.Config.Comment("阵营最大成员数量(1-50)")
+    @net.minecraftforge.common.config.Config.RangeInt(min = 1, max = 50)
+    public static int factionMaxMembers = 15;
 }
